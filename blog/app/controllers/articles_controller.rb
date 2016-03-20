05a340b618c0896a6d30a420e2d52a53ef528b46
F.Salinas <florenciabsalinas@gmail.com>
2016-03-20 13:23:19 -0400

    add changes

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -14,17 +14,30 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new   
     end
-
+    
+    def edit
+	@article = Article.find(params[:id])
+    end 
 
     def create
         @article = Article.new(article_params)
    
          if  @article.save
              redirect_to @article 
-    else
-        render 'new'
+   	 else
+        	render 'new'
+   	 end
     end
- end
+	
+    def update 
+	    @article = Article.find(params[:id])
+
+	    if @article.update(article_params)
+		    redirect_to @article
+	    else
+		    render 'edit'
+            end
+    end
 
     private
         def article_params
